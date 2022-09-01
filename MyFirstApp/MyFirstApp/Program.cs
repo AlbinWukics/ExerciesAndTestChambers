cbac2a32102a917ecbe222eedb0d2da1e017d956
AlbinWukics <112380147+AlbinWukics@users.noreply.github.com>
2022-09-01 11:37:59 +0200

    Uppgift 5

--- MyFirstApp/MyFirstApp/Program.cs
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -1,1 +1,23 @@
-﻿Console.WriteLine("Hello, World! Welcome to ITHS");
+﻿// SPARA UPPGIFT 6
+
+
+while (true)
+{
+    int sum, numberOfInputs, medelvärde;
+
+    Console.WriteLine("Mata in tal");
+    if (int.TryParse(Console.ReadLine(), out var inputNr))
+    {
+        sum = sum + inputNr;
+        numberOfInputs++;
+        Console.WriteLine($"Summan av dina tal är {sum}.");
+        Console.WriteLine(numberOfInputs);
+    }
+    else
+    {
+        medelvärde = sum / numberOfInputs;
+        Console.WriteLine($"Medelvärdet på dina inmatade tal är: {medelvärde}.");
+        Console.ReadKey(true);
+        break;
+    }
+}
